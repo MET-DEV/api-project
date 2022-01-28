0f6041fd7fa9bfff11bdbd62280200744873a201
MET-DEV <mustafatpe23@gmail.com>
2022-01-28 13:41:18 +0300

    CRUD operations added for product models

--- repositories/product_repo.go
+++ repositories/product_repo.go
@@ -5,8 +5,28 @@ import (
 	"github.com/MET-DEV/api-project/models"
 )
 
-func GetAll() []models.Product {
+func GetAllProducts() []models.Product {
 	var products []models.Product
 	config.DB.Find(&products)
 	return products
+}
+func GetByIdProduct(id int) models.Product {
+	var product models.Product
+	config.DB.First(&product, id)
+	return product
+}
+func AddProduct(product models.Product) models.Product {
+	config.DB.Create(&product)
+	return product
+
+}
+func DeleteProduct(id int) models.Product {
+	var product models.Product
+	config.DB.Where("id=?", id).Delete(&product)
+	return product
+}
+func UpdateProduct(product models.Product) models.Product {
+	config.DB.Save(&product)
+	return product
+
 }
