f86041addf6859001d4c44fe9834d5fa2703374e
MET-DEV <mustafatpe23@gmail.com>
2022-02-01 10:31:53 +0300

    validations added for product and category

--- models/product_model.go
+++ models/product_model.go
@@ -6,9 +6,9 @@ import (
 
 type Product struct {
 	gorm.Model
-	ProductName string   `json:"product_name"`
-	Description string   `json:"description"`
-	Price       float32  `json:"price"`
-	CategoryID  int      `json:"category_id"`
+	ProductName string   `json:"product_name" validate:"required" `
+	Description string   `json:"description" validate:"required"`
+	Price       float32  `json:"price" validate:"required"`
+	CategoryID  int      `json:"category_id" validate:"required"`
 	Category    Category `json:"category"`
 }
---
+++ validations/validation.go
@@ -0,0 +1,31 @@
+package validations
+
+import (
+	"github.com/MET-DEV/api-project/models"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+func ProductValidator(data models.Product) validator.FieldError {
+	v := validator.New()
+
+	err := v.Struct(data)
+	if err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			return e
+		}
+	}
+
+	return nil
+}
+func CategoryValidator(data models.Category) validator.FieldError {
+	v := validator.New()
+
+	err := v.Struct(data)
+	if err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			return e
+		}
+	}
+
+	return nil
+}
